d6c40397198ba7480afee2308f4188ba4d168e47
snert <snert@snert-lab.de>
2011-01-23 16:08:22 +0100

    Abfrage ausgebaut + Toast Messages

--- src/de/snertlab/discoserv/DiscoServActivity.java
+++ src/de/snertlab/discoserv/DiscoServActivity.java
@@ -18,10 +18,10 @@ import org.apache.http.protocol.HTTP;
 
 import android.app.Activity;
 import android.os.Bundle;
-import android.os.Handler;
 import android.util.Log;
 import android.view.View;
 import android.widget.TextView;
+import android.widget.Toast;
 
 public class DiscoServActivity extends Activity {
 	
@@ -30,15 +30,7 @@ public class DiscoServActivity extends Activity {
 	private static Pattern PATTERN_GUTHABEN = Pattern.compile("Guthaben:{1}.*<b>(.*) EUR{1} </b>{1}");
 	private DefaultHttpClient httpclient;
 	private TextView txtViewGuthaben;
-	private Handler mHandler;
 	private String betrag;
-	
-    // Create runnable for posting
-    final Runnable mUpdateResults = new Runnable() {
-        public void run() {
-            updateResultsInUi();
-        }
-    };
 	
     /** Called when the activity is first created. */
 	@Override
@@ -47,48 +39,74 @@ public class DiscoServActivity extends Activity {
         setContentView(R.layout.main);
         txtViewGuthaben = (TextView) findViewById(R.id.txtViewGuthaben);
     	httpclient = new DefaultHttpClient();
-    	mHandler = new Handler();
     }
 	    
-    public void btnRequestClickHandler(View view) {
+    public void btnRequestClickHandler(final View view) {
+    	Log.d(LOG_TAG, "btnRequestClickHandler");
     	//TODO:Pruefen ob internet verfuegbar
 //    	ConnectivityManager conManager = (ConnectivityManager) view.getContext().getSystemService(Context.CONNECTIVITY_SERVICE);
 //    	conManager.requestRouteToHost(ConnectivityManager., hostAddress)
     	betrag = "";
     	txtViewGuthaben.setText("");
+    	requestBetrag(view);
+    }
+    
+    private void requestBetrag(final View view){
     	Thread thread = new Thread(new Runnable() {
 			@Override
 			public void run() {
 				try{
+					Log.d(LOG_TAG, "requestBetrag thread startet");
+					showToast(view, "Verbinde bitte warten...");
 			    	List <NameValuePair> nvps = new ArrayList <NameValuePair>();
 			    	HttpPost httpost = new HttpPost("https://service.discoplus.de/frei/LOGIN");
 			    	nvps.add(new BasicNameValuePair("credential_0", "mobileNr"));
-			    	nvps.add(new BasicNameValuePair("credential_1", "password"));
+			    	nvps.add(new BasicNameValuePair("credential_1", "passw"));
 			    	nvps.add(new BasicNameValuePair("destination", "/discoplus/index3.php"));
 			    	httpost.setEntity(new UrlEncodedFormEntity(nvps, HTTP.UTF_8));
 			    	HttpResponse response = httpclient.execute(httpost);
-			    	if(HttpStatus.SC_OK==response.getStatusLine().getStatusCode()){
+			    	int statusCode = response.getStatusLine().getStatusCode();
+			    	if(HttpStatus.SC_OK==statusCode){
+			    		showToast(view, "Status OK");
 			    		String html 	= makeHtmlFromResponse(response);
 			    		betrag 	= findGuthaben(html);
-			    		if(betrag==null){
-			    			Log.e(LOG_TAG, "Betrag ist leer");
-			    		}else{
-			    			mHandler.post(mUpdateResults);
-			    		}
+		    			Log.d(LOG_TAG, "mHandler.post");
+		    			updateBetragText(betrag);
+			    	}else{
+			    		StringBuilder sb = new StringBuilder("Fehler falscher HTTP Status: " + statusCode);
+			    		Log.w(LOG_TAG, sb.toString());
+			    		showToast(view, sb.toString());
 			    	}
 				}catch (Exception e) {
 					Log.e(LOG_TAG, "", e);
+					showToast(view, "Fehler beim Aufbau der Verbindung");
 				}
+				Log.d(LOG_TAG, "requestBetrag thread end");
 			}
 		});
-    	thread.start();
-    }
-
-    private void updateResultsInUi(){
-    	txtViewGuthaben.setText(betrag + "Û");
+    	thread.start();    	
     }
     
+    private void showToast(final View view, final String text){
+    	Log.d(LOG_TAG, "showToast");
+		this.runOnUiThread(new Runnable() {
+		    public void run() {
+		    	Toast.makeText(view.getContext(), text, Toast.LENGTH_SHORT).show();
+		    }
+		});
+    }
+    
+    private void updateBetragText(final String betrag){
+    	Log.d(LOG_TAG, "updateBetragText");
+		this.runOnUiThread(new Runnable() {
+		    public void run() {
+		    	txtViewGuthaben.setText(betrag + "Û");
+		    }
+		});    	
+    }
+
     private String makeHtmlFromResponse(HttpResponse response){
+    	Log.d(LOG_TAG, "makeHtmlFromResponse start");
     	try{
 	    	StringBuilder sb = new StringBuilder();
 	    	BufferedReader br = new BufferedReader(new InputStreamReader(response.getEntity().getContent()));
@@ -97,19 +115,22 @@ public class DiscoServActivity extends Activity {
 	    		sb.append(readLine);
 	    	}
 	    	br.close();
+	    	Log.d(LOG_TAG, "makeHtmlFromResponse end");
 	    	return sb.toString();
     	}catch (Exception e) {
     		Log.e(LOG_TAG, "",e);
+    		throw new RuntimeException(e);
 		}
-    	return "";
     }
     
     private String findGuthaben(String html){
+    	Log.d(LOG_TAG, "findGuthaben start");
     	String betrag = null;
     	Matcher m = PATTERN_GUTHABEN.matcher(html);
     	if(m.find()){
     		betrag = m.group(1);
-    	}    		
+    	}
+    	Log.d(LOG_TAG, "findGuthaben end");
     	return betrag;
     }
     
